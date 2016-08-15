36e856ba22463550384371e8c56ed56b8f0c2ee7
Thor Watanabe <thormac@gmail.com>
2016-08-15 21:54:15 +0900

    Modified mysql setup script

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-mysql -u root -praspberry<01_db.sql
-mysql -u root -praspberry 02_user.sql
-mysql -u root -praspberry 03_grant.sql
-mysql -u root -praspberry 04_table.sql
+mysql -u root -praspberry <01_db.sql
+mysql -u root -praspberry < 02_user.sql
+mysql -u root -praspberry < 03_grant.sql
+mysql -u root -praspberry < 04_table.sql
