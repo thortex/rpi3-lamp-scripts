875b93e65ca661776b6404781f0bf9872c2566f0
Thor Watanabe <thormac@gmail.com>
2016-08-20 07:25:30 +0900

    added HTML get parameter processing to PHP script.

--- var/www/php/json.php
+++ var/www/php/json.php
@@ -1,6 +1,6 @@
 <? 
 
-print("{");
+header('Content-Type: application/json; charset=utf-8');
 
 $dbhandle = new mysqli('localhost', 'webiopi', 'raspberry');
 if ($dbhandle->connect_errno) {
@@ -10,6 +10,55 @@ if ($dbhandle->connect_errno) {
 
 $dbhandle->select_db('i2c_env_sensors');
 $query = "SELECT * FROM basics";
+
+if (($_GET['period'] != '' ) || ($_GET['start'] != '') || ($_GET['end'] != '')) {
+   $where = "";
+//   print("<p>period param:" . $_GET['period'] . "\n");
+//   print("<p>start param:" . $_GET['start'] . "\n");
+//   print("<p>end param:" . $_GET['end'] . "\n");   
+     $freq = 60; // if 1 sampling per min
+     $freq = 1;  // if 1 sampling per hour
+     switch($_GET['period']) {
+          case 'hourly':
+	       $where = " (`id` %  $freq) = 0 ";
+	       break;
+          case 'daily':
+	       $where = " (`id` % ($freq*24)) = 0 ";
+	       break;
+          case 'weekly':
+	       $where = " (`id` % ($freq*24*7)) = 0 ";
+	       break;
+          case 'monthly':
+	       $where = " (`id` % ($freq*24*30)) = 0 ";
+	       break;
+          case 'yearly':
+	       $where = " (`id` % ($freq*24*30*12)) = 0 ";
+	       break;
+     }
+
+     if ( $_GET['start'] != '') {
+     	if ($where != '') {
+	   $where .= ' AND ';
+	}
+	$where .= " `date` > '" . $_GET['start'] . "' ";
+     }
+     
+     if ( $_GET['end'] != '') {
+     	if ($where != '') {
+	   $where .= ' AND ';
+	}
+	$where .= " `date` < '" . $_GET['end'] . " 23:59:59' ";
+     }
+
+     if ($where != '') {
+     	$query .= " WHERE " . $where ;
+     }
+}
+
+//print("query: " . $query) ;
+
+$query .= " LIMIT 0, 100000";
+
 $result = $dbhandle->query($query);
 
 if (!$result) {
@@ -18,21 +67,25 @@ if (!$result) {
    exit();
 }
 
+$line_cnt = 0;
+print("[");
 while ($row = $result->fetch_assoc()) {
+      if ($line_cnt != 0) { print(','); }
       print(
-	     "{date:\"" . $row['date'] . 
-      	     "\",temp:\"" . $row['temp'] . 
-	     "\",pres:\"" . $row['pres'] . 
-	     "\",humi:\"" . $row['humi'] . 
-	     "\",lumi:\"" . $row['lumi'] . 
-	     "\",cput:\"" . $row['cput'] . 
-	     "\"}") ;
+	      '{"d":"' . $row['date'] . 
+      	     '","t":' . $row['temp'] . 
+	     ',"p":' . $row['pres'] . 
+	     ',"h":' . $row['humi'] . 
+	     ',"l":' . $row['lumi'] . 
+	     ',"c":' . $row['cput'] . 
+	     '}') ;
+      $line_cnt++;
+//      if ($line_cnt > 200) { break ; }
 }
+print("]");
 
 $result->free();
 $dbhandle->close();
-
-print("}");
 
 ?>
 
